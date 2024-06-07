2faf2224ccfac890ef2668e6c9e3ff4ecb8776cc
Ulises Cano Martinez <eziocano23@hotmail.com>
2024-06-07 14:48:02 -0600

    Send version test

--- src/pages/OrderWork.jsx
+++ src/pages/OrderWork.jsx
@@ -1,19 +1,80 @@
 import React, { useEffect, useState } from 'react'
-import { useLocation, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import { supabase, uno } from '../utils/supabase';
+import { Timeline, Button, } from 'antd';
+import { Stack } from '@chakra-ui/react';
 
-const OrderWork = ({}) => {
-    const location = useLocation();
-    const { pathname } = location;
-    console.log("🚀 ~ OrderWork ~ pathname:", pathname)
+const OrderWork = ({ }) => {
+
     const { id } = useParams();
-    console.log("🚀 ~ OrderWork ~ id:", id)
-  
-    return (
-        <div>
-            <h1>OrderWork</h1>
-            <h1>{id}</h1>
-        </div>
-    );
+    const itemId = id / uno;
+    const [item, setItem] = useState(null)
+    const [status, setStatus] = useState([])
+    const [loader, setloader] = useState(false)
+
+    useEffect(() => {
+        getData()
+    }, [itemId])
+
+    useEffect(() => {
+        if (item?.id) getStatus(item?.company_id)
+    }, [item])
+
+    async function getStatus(company_id) {
+        const { data, error } = await supabase.rpc('_get_status_ordered_by_id', { _company_id_: company_id });
+        if (error) return;
+        if (data.length > 0) {
+            const status_ = (data || []).map(item__ => {
+                let color = 'gray';
+                if (item__?.id == item?.status) color = 'blue';
+                return {
+                    color: color,
+                    children: item__?.name,
+                };
+            });
+            setStatus(status_)
+        }
+    }
+
+    const getData = async () => {
+        try {
+            let { data, error } = await supabase.from('travel').select("*").eq('id', itemId);
+            if (error) {
+                setloader(false)
+                return 0;
+            }
+            setItem(data[0])
+            setloader(true)
+        } catch (error) {
+            console.log("🚀 ~ getData ~ error:", error)
+        }
+
+    }
+
+    const upStatusOne = async (id) => {
+        const { data, error } = await supabase.from('travel').update({ status: id }).eq('id', itemId).select()
+        console.log("🚀 ~ upStatusOne ~ data:", data)
+        console.log("🚀 ~ upStatusOne ~ error:", error)
+        setItem({ ...item, status: id })
+    }
+
+
+    if (!loader) {
+        return (
+            <Stack gap={5} p={10}>
+                <h1>Cargando...</h1>
+            </Stack>
+        )
+    } else {
+        return (
+            <Stack gap={5} p={10}>
+                <h1>OrderWork OT-{itemId}</h1>
+                <Timeline items={status} />
+                <Button type="primary" onClick={() => upStatusOne(2)} disabled={item?.status > 1}>Iniciar viaje</Button>
+                <Button type="primary" onClick={() => upStatusOne(3)} disabled={item?.status == 1 || item?.status > 2}>Terminar viaje</Button>
+            </Stack>
+        )
+    }
 };
 
 export default OrderWork;
